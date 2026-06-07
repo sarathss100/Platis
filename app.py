df6c6e0f41f46e3bf6a6db6a7f951e27f7ce2eed
sarathss100 <sarathssofficial100@gmail.com>
2026-06-07 15:24:44 +0530

    feat: add part inside plate functionality

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from MetalPlate import MetalPlate
 from Rectangle import Rectangle
 
-plate = MetalPlate(1500.0, 6000.0, 0.0, 0.0)
+plate = MetalPlate(1500, 6000, 0.0, 0.0)
 
 plates = [
-    Rectangle(500.0, 300.0, 0.0, 0.0),
-    Rectangle(200.0, 400.0, 0.0, 310.0),
-    Rectangle(400.0, 500.0, 0.0, 720.0)
+    Rectangle(500, 300, 0.0, 0.0),
+    Rectangle(200, 400, 0.0, 310),
+    Rectangle(400, 500, 0.0, 720),
+    Rectangle(2000, 3000, 0.0, 1130)
 ]
 
 def calculate_utilization(plate, plates):
@@ -16,6 +17,13 @@ def calculate_utilization(plate, plates):
         total_plate_area_required += shape.area
 
     utilization_percentage = total_plate_area_required * 100 / total_plate_area_available
-    return utilization_percentage
+    return round(utilization_percentage, 2)
 
-print(round(calculate_utilization(plate, plates), 2))
+def is_part_inside_plate(plate, rectangle):
+    plate_top_right_x_mm = plate.bottom_left_x_mm + plate.length_mm
+    plate_top_right_y_mm = plate.bottom_left_y_mm + plate.width_mm
+    rectangle_top_left_x_mm = rectangle.bottom_left_x_mm + rectangle.length_mm
+    rectangle_top_left_y_mm = rectangle.bottom_left_y_mm + rectangle.width_mm
+    return  rectangle_top_left_x_mm <= plate_top_right_x_mm and rectangle_top_left_y_mm <= plate_top_right_y_mm
+
+print(is_part_inside_plate(plate, plates[2]))
